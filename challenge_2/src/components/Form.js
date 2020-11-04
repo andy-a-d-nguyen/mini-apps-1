c052328425894b009b890a9dee0d0ea2b7dbafa2
andy-a-d-nguyen <andy.a.d.nguyen@zoho.com>
2020-11-04 13:27:09 -0800

    sent axios post req, received 404

--- challenge_2/src/components/Form.js
+++ challenge_2/src/components/Form.js
@@ -1,14 +1,38 @@
 class Form extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      JSONdata: {},
+    }
+
+    this.storeInput = this.storeInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post('/', this.state.JSONdata)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  storeInput(event) {
+    this.setState({
+      JSONdata: event.target.value
+    })
   }
 
   render() {
     return (
       <form>
         <label>JSON data:</label>
-        <input type = 'textarea'/>
-        <button>Submit</button>
+        <input type = 'textarea' onChange = {this.storeInput}/>
+        <button onClick = {this.handleSubmit}>Submit</button>
       </form>
     )
   }
