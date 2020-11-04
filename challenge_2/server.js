0727dee84a1aec995cc533735362b766ebe90a37
andy-a-d-nguyen <andy.a.d.nguyen@zoho.com>
2020-11-04 13:39:03 -0800

    sent axios post req, received expected res

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.post('/', (req, res) => {
+  res.send('POST request success')
 })
 
 app.listen(port, () => {
