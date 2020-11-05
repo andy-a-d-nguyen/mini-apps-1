e4b27959d47e7df296cda4b3a065d9959bda6d79
andy-a-d-nguyen <andy.a.d.nguyen@zoho.com>
2020-11-05 06:22:50 -0800

    pseudocode a function to flatten nested JSON

---
+++ challenge_2/flatten.js
@@ -0,0 +1,15 @@
+// a function which takes in a nested obj
+const flatten = (obj) => {
+  // get keys of 1st level obj as an array
+
+  // create a var to store csv formatted data
+
+  // iterate over keys
+    // if current key's value is empty array
+      // return
+    // if current key is not an array
+      // add key's value to result
+    // if current key's value is not an empty array
+      // recursively call function
+    // add newline to result
+}
